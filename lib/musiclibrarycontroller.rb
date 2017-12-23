74e28eb70e60718c1f64bc0069b258e7bcd93ebd
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-23 04:20:10 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -14,6 +14,7 @@ class MusicLibraryController
       puts "To list all of the artists in your library, enter 'list artists'."
       puts "To list all of the genres in your library, enter 'list genres'."
       puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
@@ -77,4 +78,3 @@ def play_song
 end
 
 end
-        
