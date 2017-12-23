e2d18e0c2928efa01bb4ed5f8919cad1b4fae8a8
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-23 04:08:28 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -21,60 +21,60 @@ class MusicLibraryController
       if user_input == "list songs"
         list_songs
       elsif user_input == "list artists"
-        list_artists 
+        list_artists
       elsif user_input == "list genres"
-        list_genres 
+        list_genres
       elsif user_input == "list artist"
-        list_songs_by_artist 
+        list_songs_by_artist
       elsif user_input == "list genre"
-        list_songs_by_genre 
+        list_songs_by_genre
       elsif user_input == "play song"
-        play_song 
-      end 
-    end 
-  end 
+        play_song
+      end
+    end
+  end
 
-  def list_songs 
+  def list_songs
     Song.all.sort_by {|songs|songs.name}.each_with_index{|song,index|
     puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
-  end 
+  end
 
-  def list_artists 
+  def list_artists
     Artist.all.sort_by {|artists|artists.name}.each_with_index{|artist,index|
     puts "#{index+1}. #{artist.name}"}
-  end 
+  end
 
   def list_genres
     Genre.all.sort_by {|genres|genres.name}.each_with_index{|genre,index|
     puts "#{index+1}. #{genre.name}"}
-  end 
+  end
 
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
-    user_input = gets.strip 
+    user_input = gets.strip
     artist = Artist.find_by_name(user_input)
     artist == nil ? nil :
     artist.songs.sort_by {|songs|songs.name}.each_with_index{|song,index|
     puts "#{index+1}. #{song.name} - #{song.genre.name}"}
-  end 
+  end
 
   def list_songs_by_genre
     puts "Please enter the name of a genre:"
-user_input = gets.strip 
+user_input = gets.strip
 genre = Genre.find_by_name(user_input)
 genre == nil ? nil :
 genre.songs.sort_by {|songs|songs.name}.each_with_index{|song,index|
 puts "#{index+1}. #{song.artist.name} - #{song.name}"}
-end 
+end
 
-def play_song 
+def play_song
   puts "Which song number would you like to play?"
-  user_input = gets.to_i 
+  user_input = gets.to_i
   playlist = Song.all.sort_by {|songs|songs.name}
-  if user_input > 0 && user_input < playlist.count 
+  if user_input > 0 && user_input < playlist.count
     puts "Playing #{playlist[user_input-1].name} by #{playlist[user_input-1].artist.name}"
-  end 
-end 
+  end
+end
 
-end 
+end
         
